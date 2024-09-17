202f9a2bcf10a1d85c83907ca007e0b439efd57e
Shubham39275 <157478112+Shubham39275@users.noreply.github.com>
2024-09-17 12:35:14 +0000

    ...

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template  # to use template
 
 app = Flask(__name__)
 
 
 @app.route('/')  # route--> part of url after the domain name
 def hello_world():
-  return "Hello, world"
+  #return "Hello, world"
+  return render_template('home.html')
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)     # local host
+  app.run(host='0.0.0.0', debug=True)  # local host
